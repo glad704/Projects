9cea2f641d4bcd88289dfe12fa735a7fd2ad62de
glad704 <glad704@gmail.com>
2020-08-22 23:54:58 +0530

    Shuffle guess what function body creation

---
+++ stub_code/shuffle_guess_what.c
@@ -0,0 +1,28 @@
+void shuffle_guess_what(int bet)
+{
+	char C[3] = {'J','Q','K'};
+	printf("Shuffling.......");
+	srand(time(NULL));
+	int i;
+	for(i=0;i<5;i++)
+	{
+		int x = rand() % 3;
+		int y = rand() % 3;
+		int temp = C[x];
+		C[x] = C[y];
+		C[y] = temp;
+	}
+	int playerguess;
+	printf("What's the position of queen - 1,2 or 3?");
+	scanf("%d",&playerguess);
+	if(C[playerguess-1]=='Q')
+	{
+		cash += 2*bet;
+		printf("You win!! \nResult : \nTotal available cash : %d\n",cash);
+	}
+	else
+	{
+		cash -= bet;
+		printf("You Loose!! \nResult : \nTotal available cash : %d\n",cash);
+	}
+}
